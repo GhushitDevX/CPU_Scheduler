58cf87fd17e014d5f4784801d7de001cccff8232
Umang <152995225+Umang205@users.noreply.github.com>
2025-03-26 14:41:46 +0530

    Update ProcessInputForm.jsx

--- frontend/src/components/ProcessInputForm.jsx
+++ frontend/src/components/ProcessInputForm.jsx
@@ -29,72 +29,72 @@ const ProcessInputForm = ({ algorithm, isPreemptive, processes, onProcessesChang
   return (
     <div>
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-bold">Process Details</h2>
+        <h2 className="text-2xl font-bold font-mono my-2 uppercase">Process Details:</h2>
         <button
           onClick={addProcess}
-          className="flex items-center space-x-1 text-blue-400 hover:text-blue-300"
+          className="flex items-center space-x-1 cursor-pointer text-xl  text-blue-500 hover:text-blue-400"
         >
           <PlusCircle size={20} />
           <span>Add Process</span>
         </button>
       </div>
 
-      <div className="bg-gray-900 rounded-lg p-4 overflow-x-auto">
+      <div className="border border-neutral-500 rounded-md overflow-x-auto">
         <table className="w-full">
-          <thead>
-            <tr className="text-left border-b border-gray-700">
-              <th className="pb-2 px-4">Process ID</th>
-              <th className="pb-2 px-4">Arrival Time</th>
-              <th className="pb-2 px-4">Burst Time</th>
-              {algorithm === 'Priority' && <th className="pb-2 px-4">Priority</th>}
-              <th className="pb-2 px-4 w-16">Actions</th>
+          <thead className='border-b border-neutral-500'>
+            <tr className="text-left font-mono text-lg">
+              <th className="py-4 px-4 border-r text-xl border-neutral-500">Process ID</th>
+              <th className="py-4 px-4 border-r text-xl border-neutral-500">Arrival Time</th>
+              <th className="py-4 px-4 text-xl ">Burst Time</th>
+              {algorithm === 'Priority' && <th className="py-4 px-4 text-xl border-l border-neutral-500">Priority</th>}
+              <th className="py-4 px-4 w-16 text-xl border-l border-neutral-500">Actions</th>
             </tr>
           </thead>
           <tbody>
             {processes.map((process, index) => (
-              <tr key={index} className="border-b border-gray-800 hover:bg-gray-800">
-                <td className="py-3 px-4">
+              <tr key={index} className=" hover:bg-gray-800">
+                <td className="py-4 px-4 border-r border-neutral-500">
                   <input
                     type="text"
                     value={process.id}
                     onChange={(e) => updateProcess(index, 'id', e.target.value)}
-                    className="bg-gray-700 px-3 py-1 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    className="bg-gray-700 px-3 py-2 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
                 </td>
-                <td className="py-3 px-4">
+                <td className="py-4 px-4 border-r border-neutral-500">
                   <input
                     type="number"
                     min="0"
                     value={process.arrivalTime}
                     onChange={(e) => updateProcess(index, 'arrivalTime', e.target.value)}
-                    className="bg-gray-700 px-3 py-1 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    className="bg-gray-700 px-3 py-2 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
                 </td>
-                <td className="py-3 px-4">
+                <td className="py-4 px-4 border-r border-neutral-500">
                   <input
                     type="number"
                     min="1"
                     value={process.burstTime}
                     onChange={(e) => updateProcess(index, 'burstTime', e.target.value)}
-                    className="bg-gray-700 px-3 py-1 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
+                    className="bg-gray-700 px-3 py-2 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
                   />
                 </td>
                 {algorithm === 'Priority' && (
-                  <td className="py-3 px-4">
+                  <td className="py-4 px-4 border-r border-neutral-500">
                     <input
                       type="number"
                       min="1"
                       value={process.priority}
                       onChange={(e) => updateProcess(index, 'priority', e.target.value)}
-                      className="bg-gray-700 px-3 py-1 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
+                      className="bg-gray-700 px-3 py-2 rounded w-full focus:outline-none focus:ring-1 focus:ring-blue-500"
                     />
                   </td>
                 )}
-                <td className="py-3 px-4">
+                <td className="flex justify-center">
                   <button
                     onClick={() => removeProcess(index)}
                     disabled={processes.length <= 1}
-                    className="text-red-400 hover:text-red-300 disabled:opacity-30"
+                    className="text-red-400 cursor-pointer mt-6 hover:text-red-300 disabled:opacity-30"
                   >
                     <Trash2 size={18} />
                   </button>
